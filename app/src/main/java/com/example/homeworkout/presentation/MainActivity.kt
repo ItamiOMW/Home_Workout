31e7c3f8cdc1bfcfc1246591530c19ba6671a165
Вадим <itamiomw@gmail.com>
2022-09-06 16:09:01 +0300

    created folders for future classes

--- app/src/main/java/com/example/homeworkout/presentation/MainActivity.kt
+++ app/src/main/java/com/example/homeworkout/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.homeworkout
+package com.example.homeworkout.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homeworkout.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
