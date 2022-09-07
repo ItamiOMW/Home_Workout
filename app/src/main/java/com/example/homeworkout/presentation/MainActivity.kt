dcdf5a2e0f376fa081b4e0b0aea4047ad3adea2c
Вадим <itamiomw@gmail.com>
2022-09-07 11:55:37 +0300

    created splash screen and added images

--- app/src/main/java/com/example/homeworkout/presentation/MainActivity.kt
+++ app/src/main/java/com/example/homeworkout/presentation/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.homeworkout.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.homeworkout.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
