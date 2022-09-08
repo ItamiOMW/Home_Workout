116c75a0aa969bc8acaf8d000d461b15717d6877
Вадим <itamiomw@gmail.com>
2022-09-08 15:49:36 +0300

    created exercise and planned workout adapters

---
+++ app/src/main/java/com/example/homeworkout/presentation/adapters/exercises_adapter/ExerciseAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.homeworkout.presentation.adapters.exercises_adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.fitness.presentation.adapters.exercises_adapter.ExerciseViewHolder
+import com.example.homeworkout.databinding.ExerciseItemBinding
+import com.example.homeworkout.domain.models.ExerciseModel
+import javax.inject.Inject
+
+class ExerciseAdapter @Inject constructor (): ListAdapter<ExerciseModel, ExerciseViewHolder>(
+    DiffUtilItemCallbackExercise()
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
+        val binding = ExerciseItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ExerciseViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
+        val item = currentList[position]
+        with(holder.binding) {
+            tvTitle.text = item.title
+            tvReps.text = item.reps.toString()
+            //TODO ADD GIFS FOR ivExerciseGif
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/homeworkout/presentation/adapters/exercises_adapter/ExerciseViewHolder.kt
@@ -0,0 +1,6 @@
+package com.example.fitness.presentation.adapters.exercises_adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.homeworkout.databinding.ExerciseItemBinding
+
+class ExerciseViewHolder(val binding: ExerciseItemBinding): RecyclerView.ViewHolder(binding.root)
