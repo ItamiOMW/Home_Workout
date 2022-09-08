4e075ca710303de8c3d45e72ba7c9da6e694384b
Вадим <itamiomw@gmail.com>
2022-09-08 13:26:55 +0300

    created workouts adapter

---
+++ app/src/main/java/com/example/homeworkout/presentation/screens/adapters/workouts_adapter/WorkoutViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.homeworkout.presentation.screens.adapters.workouts_adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.homeworkout.databinding.WorkoutItemBinding
+
+
+class WorkoutViewHolder(val binding: WorkoutItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+}
