6ba45e9dddf1603d5a7c268dc66e73e646f4567e
Вадим <itamiomw@gmail.com>
2022-09-09 15:52:36 +0300

    implemented Calendar Screen

--- app/src/main/java/com/example/homeworkout/di/modules/ViewModelModule.kt
+++ app/src/main/java/com/example/homeworkout/di/modules/ViewModelModule.kt
@@ -2,6 +2,7 @@ package com.example.homeworkout.di.modules
 
 import androidx.lifecycle.ViewModel
 import com.example.homeworkout.di.annotations.ViewModelKey
+import com.example.homeworkout.presentation.screens.calendar_screen.CalendarViewModel
 import com.example.homeworkout.presentation.screens.choose_workout_screen.ChooseWorkoutViewModel
 import dagger.Binds
 import dagger.Module
@@ -14,4 +15,9 @@ interface ViewModelModule {
     @IntoMap
     @ViewModelKey(ChooseWorkoutViewModel::class)
     fun bindChooseWorkoutViewModel(viewModel: ChooseWorkoutViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(CalendarViewModel::class)
+    fun bindCalendarViewModel(viewModel: CalendarViewModel): ViewModel
 }
